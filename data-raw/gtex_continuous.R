0c5437ed9e9b00d8234b29418b5f4943f862156b
Andrew Chen <andrewdchen@berkeley.edu>
2025-10-15 15:42:47 -0400

    gtex continuous protected

--- data-raw/gtex_continuous.R
+++ data-raw/gtex_continuous.R
@@ -3,132 +3,94 @@ library(data.table)
 library(dplyr)
 library(DESeq2)
 library(rlang)
+library(Biobase)
+options(box.path=file.path(Sys.getenv("CBMGIT"), "MLscripts"))
+box::use(R/rm_low_rnaseq_counts)
 
-# On the SCC DATA_PATH can be set to "montilab-p/CBMrepositoryData/GTEX/raw/transcriptomics/"
+# On the SCC DATA_PATH can be set to "/restricted/projectnb/gtex/montilab/CBMrepositoryData/GTEX_dbGap/processed_data"
 DATA_PATH <- ""
 PATH <- ""
 
-# 1. Load data
-# Download from https://gtexportal.org/home/downloads/adult-gtex/bulk_tissue_expression
-expr <- fread(file.path(DATA_PATH, "GTEx_Analysis_v10_RNASeQCv2.4.2_gene_reads.gct.gz"))
-# DESeq expects a dense matrix
-expr_matrix <- as.matrix(expr[, -(1:2), with=FALSE])
-rownames(expr_matrix) <- expr$Name
+# Protected data requires dbGAP application (https://www.gtexportal.org/home/protectedDataAccess)
+filepaths <- Sys.glob(paste0(DATA_PATH, "/*.rds"))
+deseq_dfs <- list()
 
-## Load sample attributes
-## Download from https://gtexportal.org/home/downloads/adult-gtex/metadata
-sample_attr <- fread(file.path(DATA_PATH, "GTEx_Analysis_v10_Annotations_SampleAttributesDS.txt"))
-## Load subject phenotypes
-subject_pheno <- fread(file.path(DATA_PATH, "GTEx_Analysis_v10_Annotations_SubjectPhenotypesDS.txt"))
-sample_attr$SUBJID <- lapply(strsplit(sample_attr$SAMPID, "-"),
-                             function(x) paste(x[1:2], collapse = "-")) %>% unlist
-## Add subject phenotypes to sample metadata
-gtex_metadata <- dplyr::left_join(sample_attr, subject_pheno, by = "SUBJID")
-gtex_metadata$AGE_ordinal <- with(gtex_metadata, dplyr::case_when(is.na(AGE) ~ NA,
-                                                                  AGE == "20-29" ~ 1,
-                                                                  AGE == "30-39" ~ 2,
-                                                                  AGE == "40-49" ~ 3,
-                                                                  AGE == "50-59" ~ 4,
-                                                                  AGE == "60-69" ~ 5,
-                                                                  AGE == "70-79" ~ 6,
-))
+for (filepath in filepaths) {
+  filename <- basename(filepath)
+  tissue_name <- sub("^RNAseq_GTEx_v10_(.*)\\.rds$", "\\1", filename)
+  print(paste0("Processing ", tissue_name))
 
-# 2. GTEx Analysis Parameters
-## Which tissues to filter for.
-## Some are in the SMTSD field, others are in the SMTS field.
-tissue_filters <- list(blood = c("SMTSD", "Whole Blood"),
-                       brain = c("SMTS", "Brain"))
+  # 1. Load Data
+  expr <- readRDS(filepath)
+  # DESeq expects a dense matrix
+  expr_matrix <- as.matrix(expr@assays@data$counts)
 
-gtex_sample_ids <- function(gtex_metadata,
-                            tissue_column,
-                            tissue_name) {
-
-  sample_ids <- gtex_metadata %>%
-    dplyr::filter(.data[[tissue_column]] == tissue_name) %>%
-    dplyr::pull(SAMPID)
-  return(sample_ids)
-}
-
-gtex_deseq_helper <- function(gtex_expr,
-                              gtex_metadata,
-                              tissue_filters) {
-
-  # Get gtex subsets
-  deseq_dfs <- list()
-  for(tissue_exp in names(tissue_filters)) {
-    tissue_filter <- tissue_filters[[tissue_exp]]
-    tissue_col_name <- tissue_filter[1]
-    tissue_name <- tissue_filter[2]
-
-    samp_ids <- gtex_sample_ids(gtex_metadata,
-                                tissue_col_name,
-                                tissue_name)
-
-    # 1. Get the intersection of available sample IDs
-    all_samp_ids <- intersect(colnames(gtex_expr), samp_ids)
-
-    # 2. Subset expression matrix to matched and ordered sample IDs
-    expr_sub <- gtex_expr[, all_samp_ids]
-
-    # 3. Subset and order metadata to match expression matrix columns
-    expr_metadata <- gtex_metadata %>%
-      dplyr::filter(SAMPID %in% all_samp_ids) %>%
-      dplyr::arrange(match(SAMPID, all_samp_ids))
-
-    # 4. Convert categorical covariates to factors
-    expr_metadata$SEX <- factor(expr_metadata$SEX)
-    expr_metadata$SMCENTER <- factor(expr_metadata$SMCENTER)
-
-    # 5. Center and scale numerical covariates
-    expr_metadata$SMRIN <- scale(expr_metadata$SMRIN)
-    expr_metadata$SMTSISCH <- scale(expr_metadata$SMTSISCH)
-
-    # 6. Remove samples with missing data in any covariate
-    complete_idx <- complete.cases(expr_metadata[, c("AGE_ordinal", "SEX", "SMRIN", "SMCENTER", "SMTSISCH")])
-    expr_metadata <- expr_metadata[complete_idx, ]
-    expr_sub <- expr_sub[, complete_idx]
-
-    print(tissue_name)
-    print(table(expr_metadata$AGE))
-
-    # DESeq2 analysis
-    dds <- DESeqDataSetFromMatrix(
-      countData = round(expr_sub),
-      colData = expr_metadata,
-      design = ~ AGE_ordinal + SEX + SMRIN + SMCENTER + SMTSISCH
-    ) %>% DESeq()
-
-    deseq_df <- results(dds, name = "AGE_ordinal")
-    deseq_df <- deseq_df %>%
-      as.data.frame %>%
-      tidyr::drop_na() %>%
-      dplyr::mutate(logFC_pval = log2FoldChange*(-log10(padj))) %>%
-      dplyr::arrange(desc(logFC_pval))
-
+  # Check if there's enough samples for deseq.
+  if(ncol(expr) < 10) {
+    print(paste0("Fewer than 10 samples, skipping ", tissue_name))
     deseq_name <- tissue_name
-    deseq_dfs[[deseq_name]] <- deseq_df
+    deseq_dfs[[deseq_name]] <- NULL
+    next
   }
-  return(deseq_dfs)
+
+  # 4. Convert categorical covariates to factors
+  expr$SEX <- factor(expr$SEX)
+  expr$SMCENTER <- factor(expr$SMCENTER)
+
+  # 5. Center and scale numerical covariates
+  expr$SMRIN <- scale(expr$SMRIN)
+  expr$SMTSISCH <- scale(expr$SMTSISCH)
+
+  # 6. Remove samples with missing data in any covariate
+  # List of candidate covariates
+  covariates <- c("AGE", "SEX", "SMRIN", "SMCENTER", "SMTSISCH")
+  expr_metadata <- expr@colData %>% data.frame()
+  # Check which covariates have more than one unique value
+  valid_covariates <- covariates[sapply(expr_metadata[, covariates], function(x) length(unique(x)) > 1)]
+  print(valid_covariates)
+  design_formula <- as.formula(
+    paste("~", paste(valid_covariates, collapse = " + "))
+  )
+  complete_idx <- complete.cases(expr_metadata[, valid_covariates])
+  expr_metadata <- expr_metadata[complete_idx, ]
+  expr_sub <- expr_matrix[, complete_idx]
+
+  print(table(expr_metadata$AGE))
+
+  # DESeq2 analysis
+  dds <- DESeqDataSetFromMatrix(
+    countData = round(expr_sub),
+    colData = expr_metadata,
+    design = design_formula
+  ) %>% DESeq()
+
+  deseq_df <- results(dds, name = "AGE")
+  deseq_df <- deseq_df %>%
+    as.data.frame %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(logFC_pval = log2FoldChange*(-log10(padj))) %>%
+    dplyr::arrange(desc(logFC_pval))
+
+  deseq_name <- tissue_name
+  deseq_dfs[[deseq_name]] <- deseq_df
 }
 
-deseq_dfs <- gtex_deseq_helper(gtex_expr = expr_matrix,
-                               gtex_metadata = gtex_metadata,
-                               tissue_filters = tissue_filters)
-saveRDS(deseq_dfs, file.path(PATH, "deseq_cont_dfs.rds"))
-gtex.blood.aging <- list(up = deseq_dfs$`Whole Blood` %>%
-                           dplyr::filter(log2FoldChange > 0) %>%
-                           dplyr::slice(1:100) %>%
-                           rownames,
-                         up_full = deseq_dfs$`Whole Blood` %>%
-                           rownames)
-gtex.brain.aging <- list(up = deseq_dfs$Brain %>%
-                           dplyr::filter(log2FoldChange > 0) %>%
-                           dplyr::slice(1:100) %>%
-                           rownames,
-                         up_full = deseq_dfs$Brain %>%
-                           rownames)
+saveRDS(deseq_dfs, file.path(PATH, "/data/gtex/deseq_cont_dfs_all.rds"))
 
-vennr::vennr(list(blood = gtex.blood.aging$up,brain = gtex.brain.aging$up))
-usethis::use_data(gtex.blood.aging, overwrite = TRUE)
-usethis::use_data(gtex.brain.aging, overwrite = TRUE)
+deseq_cont_dfs_all <- readRDS(file.path(PATH, "/data/gtex/deseq_cont_dfs_all.rds"))
 
+gtex_cont_sigs <- list()
+for(tissue in names(deseq_cont_dfs_all)) {
+  deseq_df <- deseq_cont_dfs_all[[tissue]]
+  tissue_up <- deseq_df %>%
+    dplyr::filter(log2FoldChange > 0) %>%
+    dplyr::slice(1:100) %>%
+    rownames
+  tissue_up_full <- deseq_df %>%
+    rownames
+  gtex_cont_sigs[[tissue]][["up"]] <- tissue_up
+  gtex_cont_sigs[[tissue]][["up_full"]] <- tissue_up_full
+}
+saveRDS(gtex_cont_sigs, file.path(PATH, "all_tissue_sigs.rds"))
+gtex.aging <- gtex_cont_sigs
+usethis::use_data(gtex.aging)
